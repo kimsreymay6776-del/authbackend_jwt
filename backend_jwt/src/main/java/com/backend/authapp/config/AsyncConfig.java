fd9eb7093e3e0b259b7d58bde64cc3d4f7aff98d
vinsolang <vinsolang9@gmail.com>
2026-09-17 14:42:04 +0700

    update file example to backend

---
+++ backend_jwt/src/main/java/com/backend/authapp/config/AsyncConfig.java
@@ -0,0 +1,22 @@
+package com.backend.authapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
+
+// public class AsyncConfig {
+// }
+
+// package com.backend.authapp.config;
+
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.scheduling.annotation.EnableAsync;
+
+// @Configuration
+// @EnableAsync
+// public class AsyncConfig {
+// }
